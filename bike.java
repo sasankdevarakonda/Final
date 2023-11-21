14ae3f383a5bf56300546663530b1009be206930
sasankdevarakonda <149362760+sasankdevarakonda@users.noreply.github.com>
2023-11-21 11:55:26 +0530

    cannot assign a values in final key word

---
+++ bike.java
@@ -0,0 +1,12 @@
+public class bike {
+
+        final int speed=90;
+        void run(){
+            speed=85;
+        }
+        public static void main(String args[]){
+            bike obj=new  bike();
+            obj.run();
+        }
+
+}
